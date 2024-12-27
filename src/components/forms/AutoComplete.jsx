352636a51f1eecb37b6783aa49e6a5eda3cafd3f
roshiroku <shir.oraan@gmail.com>
2024-12-27 20:52:49 +0200

    fix select input

--- src/components/forms/AutoComplete.jsx
+++ src/components/forms/AutoComplete.jsx
@@ -1,8 +1,22 @@
+import { useRef } from 'react';
+
 const AutoComplete = ({ options, value, onChange, multiple = false }) => {
+  const optionElements = useRef([]);
+
+  const handleChange = (e) => {
+    if (multiple) {
+      const values = [];
+      optionElements.current.forEach(({ selected, value }) => selected && values.push(value));
+      onChange(values);
+    } else {
+      onChange(e.target.value);
+    }
+  };
+
   return (
-    <select value={value} onChange={onChange} multiple={multiple}>
-      {options.map(([value, text]) => (
-        <option key={value ?? text} value={value ?? text}>
+    <select value={value} onChange={handleChange} multiple={multiple}>
+      {options.map(([value, text], i) => (
+        <option key={value ?? text} value={value ?? text} ref={el => optionElements.current[i] = el}>
           {text ?? value}
         </option>
       ))}
